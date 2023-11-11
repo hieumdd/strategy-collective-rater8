34594733dbd36b731d732dadc44964631477408e
HM <hieumdd@gmail.com>
2023-11-11 21:49:36 +0700

    feat: Add createReviewPipelines

--- src/pipeline/pipeline.service.ts
+++ src/pipeline/pipeline.service.ts
@@ -1,11 +1,13 @@
 import { Readable } from 'node:stream';
 import { pipeline } from 'node:stream/promises';
 import ndjson from 'ndjson';
+import pMap from 'p-map';
 
 import { logger } from '../logging.service';
-import { createLoadStream } from '../bigquery.service';
-import { getPractices, getReviews } from '../rater8/rater8.service';
+import { LoadConfig, createLoadStream } from '../bigquery.service';
+import { getPractices, getReviewsByEmployee, getReviewsByLocation } from '../rater8/rater8.service';
 import * as pipelines from './pipeline.const';
+import { Practice } from '../rater8/rater8.type';
 
 export const runPipeline = async () => {
     const practices = await getPractices();
@@ -16,27 +18,46 @@ export const runPipeline = async () => {
         createLoadStream(pipelines.Practice),
     );
 
-    const reviewStream = new Readable({ objectMode: true, read: () => {} });
+    const createReviewPipeline = async <T>(
+        paramsFn: (practices: Practice[]) => T[],
+        get: (params: T) => Promise<object[]>,
+        loadConfig: LoadConfig,
+    ) => {
+        const reviewStream = new Readable({ objectMode: true, read: () => {} });
 
-    const reviewPipeline = pipeline(reviewStream, ndjson.stringify(), createLoadStream(pipelines.Review));
-
-    Promise.all(
-        practices.flatMap((practice) => {
-            return practice.locations.map(async (location) => {
-                return getReviews({
-                    practiceId: practice.id,
-                    locationId: location.id,
-                }).then((reviews) => {
-                    (reviews || []).forEach((review) => reviewStream.push(review));
-                });
+        pMap(
+            paramsFn(practices),
+            async (params) => {
+                logger.debug({ fn: 'runPipeline', params });
+                return await get(params).then((reviews) => reviews.forEach((review) => reviewStream.push(review)));
+            },
+            { concurrency: 20 },
+        )
+            .then(() => reviewStream.push(null))
+            .catch((error) => {
+                logger.error({ fn: 'runPipeline', error });
+                reviewStream.emit('error', error);
             });
-        }),
-    )
-        .then(() => reviewStream.push(null))
-        .catch((error) => {
-            logger.error({ fn: 'runPipeline', error });
-            reviewStream.emit('error', error);
-        });
 
-    return Promise.all([practicePipeline, reviewPipeline]).then(() => true);
+        return pipeline(reviewStream, ndjson.stringify(), createLoadStream(loadConfig));
+    };
+
+    const reviewByLocationPipeline = createReviewPipeline(
+        (practices) =>
+            practices.flatMap((practice) => {
+                return practice.locations.map((location) => ({ practiceId: practice.id, locationId: location.id }));
+            }),
+        getReviewsByLocation,
+        pipelines.LocationReview,
+    );
+    const reviewByEmployeePipeline = createReviewPipeline(
+        (practices) =>
+            practices.flatMap((practice) => {
+                return practice.employees.map((employee) => ({ practiceId: practice.id, employeeId: employee.id }));
+            }),
+        getReviewsByEmployee,
+        pipelines.EmployeeReview,
+    );
+
+    return Promise.all([practicePipeline, reviewByLocationPipeline, reviewByEmployeePipeline]).then(() => true);
 };
